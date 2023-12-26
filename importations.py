b86ed01f2e78017f533f1fcf0650787025a85513
FelixGomes <gustavogomes50@gmail.com>
2023-12-26 20:02:37 -0300

    First Commit

---
+++ importations.py
@@ -0,0 +1,4 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import requests
---
+++ script_extracao.py
@@ -0,0 +1,37 @@
+import importations
+from bs4 import BeautifulSoup
+import requests
+import login
+
+#Global Variables
+driver = importations.webdriver.Chrome()
+
+#Recebe as credenciais do usuário e faz login
+def user_login():
+    print("Você iniciou o script de extração de dados! \nInforme o link da página de login e depois digite suas credenciais para podermos coletar os dados e fazer o download no seu PC")
+    url_login = input('\nLogin website: ')
+    print('\nAgora digite suas credenciais:\n')
+    username = input('username = ')
+    password = input('password = ')
+
+    login.login_at_page(url_login, username, password)
+    
+    extract_files()
+
+#Extrai os arquivos
+def  extract_files():
+    url_source_website = input("Digite o URL do qual deseja extrair os arquivos: ")
+    
+
+    driver.get(url_source_website)
+
+    #to see the whole html content at command line
+    html = requests.get(url_source_website).content
+    soup = BeautifulSoup(html, 'html.parser')
+    print(soup.prettify())
+
+    driver.close()
+
+user_login()
+
+
